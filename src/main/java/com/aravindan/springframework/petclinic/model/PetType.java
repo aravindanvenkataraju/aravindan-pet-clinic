ebcf721c5a7c26124afc613e0eebc4d950922ca5
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-26 17:18:13 +1100

    create POJOs. closes #2

---
+++ src/main/java/com/aravindan/springframework/petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.aravindan.springframework.petclinic.model;
+
+public class PetType {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
