aa871905b29c9308e2ad58859d98107ebd26d38a
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-26 18:12:29 +1100

    add-base-entity-with-id-property. closes #4

---
+++ aravindan-pet-clinic-data/src/main/java/com/aravindan/springframework/petclinic/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.aravindan.springframework.petclinic.model;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class BaseEntity implements Serializable {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+
+}
