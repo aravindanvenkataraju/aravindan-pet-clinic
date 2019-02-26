54925c9959b52b1d7129375cecd408eda4a7b209
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-26 21:23:22 +1100

    add-owners-index-page-and-controller. closes #13

--- aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/controllers/OwnersIndexController.java
+++ aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/controllers/OwnersIndexController.java
@@ -2,12 +2,12 @@ package com.aravindan.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetsIndexController {
+public class OwnersIndexController {
 	
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
-		return "vets/index";
+	public String listOwners() {
+		return "owners/index";
 	}
 }
