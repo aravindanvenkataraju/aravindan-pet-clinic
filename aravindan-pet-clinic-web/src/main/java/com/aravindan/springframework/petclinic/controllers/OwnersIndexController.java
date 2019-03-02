4161ce0f82a70895a0a8d5facf6d415b3cc55fee
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-03-02 11:17:42 +1100

    list owners to owners index page. closes #16

--- aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/controllers/OwnersIndexController.java
+++ aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/controllers/OwnersIndexController.java
@@ -1,13 +1,26 @@
 package com.aravindan.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aravindan.springframework.petclinic.services.OwnerService;
 @RequestMapping("/owners")
 @Controller
 public class OwnersIndexController {
 	
+	private final OwnerService ownerService;
+	
+	
+	public OwnersIndexController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
