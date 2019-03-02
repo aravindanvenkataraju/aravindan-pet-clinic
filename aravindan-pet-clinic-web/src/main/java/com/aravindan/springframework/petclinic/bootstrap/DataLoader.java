adf974c29779bd860b0f9db08cb4b3166e3d0eca
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-03-02 11:47:43 +1100

    add logic to set ID. closes #23

--- aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/bootstrap/DataLoader.java
+++ aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/bootstrap/DataLoader.java
@@ -23,13 +23,11 @@ public class DataLoader implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Aravindan");
 		owner1.setLastName("Venkataraju");
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Kamal");
 		owner2.setLastName("Hassan");
 		ownerService.save(owner2);
@@ -37,13 +35,11 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Owners Loaded...");
 		
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Vikram");
 		vet1.setLastName("Kenny");
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Vijay");
 		vet2.setLastName("Ilayathalapathy");
 		vetService.save(vet2);
