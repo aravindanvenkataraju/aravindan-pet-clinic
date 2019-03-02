d071e23e73c7bbf4a4054a229a6f335c32a2c735
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-03-02 11:29:37 +1100

    add vets list to the vets index page. closes #15

--- aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/controllers/VetsIndexController.java
+++ aravindan-pet-clinic-web/src/main/java/com/aravindan/springframework/petclinic/controllers/VetsIndexController.java
@@ -1,13 +1,24 @@
 package com.aravindan.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aravindan.springframework.petclinic.services.VetService;
 @RequestMapping("/vets")
 @Controller
 public class VetsIndexController {
 	
+	private final VetService vetService;
+	
+	public VetsIndexController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
